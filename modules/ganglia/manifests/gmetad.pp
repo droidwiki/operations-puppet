d1e26f55cf944bf6319c8f3d00674cf6fe51829c
root <root@go2tech.de>
2016-07-12 19:25:26 +0200

    Massive overhaul of puppet manifests
    * Removed unnecessary modules
    * Updated modules
    * Tested with latest puppet release
    * Rewrote modules to match new autoloading
    * Rewrite site.pp to reflect new servers
    * Rewrite roles:
    ** currently only ganglia is really puppetized
    ** puppetmaster module ensures, that puppet-lint is installed (should be moved
       to a role named jenkins or something else)
    ** default role ensures, that user florian exists and that /etc/hosts is the same
       on all hosts (containing all actual hosts)
    ** /etc/iptables.local is managed by puppet
    
    Change-Id: Ibbe23e72855a4c774e3d7ac2c2526a7f343376e8

--- modules/ganglia/manifests/gmetad.pp
+++ modules/ganglia/manifests/gmetad.pp
@@ -1,84 +1,61 @@
 # == Class: ganglia::gmetad
 #
-# install and configure the ganglia gmetad daemon
-#
-# === Parameters
-#
-# All parameteres are optional.
-#
-# [*all_trusted*]
-#   boolean, default false; if set to true, we will allow all hosts that 
-#   can query ganglia data via the xml query port.  Corresponds to the 
-#   'all_trusted' field in gmetad.conf.
-#
-# [*clusters*]
-#   array of hashes.  Valid keys are:
-#
-#   -name
-#   -polling_interval
-#   -address
-#
-#   defaults to:
-#   [ { name => 'my cluster', address => 'localhost' } ],
-#
-# [*gridname*]
-#   string - defaults to '', which means no gridname at all
-#
-# [*trusted_hosts*]
-#   array of strings, each of which matches a hostname that is allowed to
-#   query ganglia data via the xml query port.  Corresponds to the
-#   'trusted_hosts' field in gmetad.conf.
-#
-# === Examples
-#
-#
-#    $clusters = [
-#      {
-#        name     => 'test',
-#        address  => ['test1.example.org', 'test2.example.org'],
-#      },
-#    ]
-#
-#    class{ 'ganglia::gmetad':
-#      clusters => $clusters,
-#      gridname => 'my grid',
-#    }
-#
-#
-# === Authors
-#
-# Joshua Hoblitt <jhoblitt@cpan.org>
-#
-# === Copyright
-#
-# Copyright (C) 2012-2013 Joshua Hoblitt
-#
-
 class ganglia::gmetad(
-  $all_trusted = false,
-  $clusters = [ { 'name' => 'my cluster', 'address' => 'localhost' } ],
-  $gridname = undef,
-  $rras = $ganglia::params::rras,
-  $trusted_hosts = [],
-  $gmetad_package_name = $ganglia::params::gmetad_package_name,
-  $gmetad_service_name = $ganglia::params::gmetad_service_name,
-  $gmetad_service_config = $ganglia::params::gmetad_service_config,
-  $gmetad_user = $ganglia::params::gmetad_user,
-  $gmetad_case_sensitive_hostnames = $ganglia::params::gmetad_case_sensitive_hostnames # lint:ignore:80chars
+  $all_trusted                     = false,
+  $clusters                        = [
+    { 'name' => 'my cluster', 'address' => 'localhost' },
+  ],
+  $gridname                        = undef,
+  $rras                            = $::ganglia::params::rras,
+  $trusted_hosts                   = [],
+  $gmetad_package_name             = $::ganglia::params::gmetad_package_name,
+  $gmetad_service_name             = $::ganglia::params::gmetad_service_name,
+  $gmetad_service_config           = $::ganglia::params::gmetad_service_config,
+  $gmetad_user                     = $::ganglia::params::gmetad_user,
+  $gmetad_case_sensitive_hostnames =
+    $::ganglia::params::gmetad_case_sensitive_hostnames
 ) inherits ganglia::params {
+  validate_bool($all_trusted)
   ganglia_validate_clusters($clusters)
   validate_string($gridname)
   ganglia_validate_rras($rras)
+  validate_array($trusted_hosts)
   validate_string($gmetad_package_name)
   validate_string($gmetad_service_name)
   validate_string($gmetad_service_config)
   validate_string($gmetad_user)
-  validate_array($trusted_hosts)
-  validate_bool($all_trusted)
+  validate_integer($gmetad_case_sensitive_hostnames, 1, 0)
 
-  anchor{ 'ganglia::gmetad::begin': } ->
-  class{ 'ganglia::gmetad::install': } ->
-  class{ 'ganglia::gmetad::config': } ->
-  class{ 'ganglia::gmetad::service': } ->
-  anchor{ 'ganglia::gmetad::end': }
+  if ($::ganglia::params::gmetad_status_command) {
+    $hasstatus = false
+  } else {
+    $hasstatus = true
+  }
+
+  if versioncmp($::puppetversion, '3.6.0') > 0 {
+    package { $::ganglia::params::gmetad_package_name:
+      ensure        => present,
+      allow_virtual => false,
+    }
+  } else {
+    package { $::ganglia::params::gmetad_package_name:
+      ensure => present,
+    }
+  }
+
+  Package[$::ganglia::params::gmetad_package_name] ->
+  file { $::ganglia::params::gmetad_service_config:
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template($::ganglia::params::gmetad_service_erb),
+  } ~>
+  service { $::ganglia::params::gmetad_service_name:
+    ensure     => running,
+    hasstatus  => $hasstatus,
+    hasrestart => true,
+    enable     => true,
+    status     => $::ganglia::params::gmetad_status_command,
+  }
 }
--- modules/ganglia/spec/classes/gmond_spec.rb
+++ modules/ganglia/spec/classes/gmond_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'ganglia::gmond' do
-  let(:facts) {{ :osfamily => 'RedHat', :operatingsystemmajrelease => '6' }}
+  let(:facts) {{ :osfamily => 'RedHat', :operatingsystemmajrelease => '6', :puppetversion => Puppet.version }}
 
   context 'default params' do
     it 'should manage gmond.conf' do
@@ -13,9 +13,11 @@ describe 'ganglia::gmond' do
       )
     end
 
-    it 'should notify Class[ganglia::gmond::service]' do
+    it do
       should contain_file('/etc/ganglia/gmond.conf').
-        that_notifies('Class[ganglia::gmond::service]')
+        that_notifies('Service[gmond]')
+      should contain_package('ganglia-gmond').
+        with_ensure('present')
     end
 
     it 'should have default values in gmond.conf template' do
@@ -28,7 +30,8 @@ describe 'ganglia::gmond' do
         with_content(/^  latlong = "unspecified"$/).
         with_content(/^  url = "unspecified"$/).
         with_content(/^  location = "unspecified"$/).
-        without_content(/^  override_hostname =/)
+        without_content(/^  override_hostname =/).
+        without_content(/^  mcast_if/)
     end
   end # default params
 
@@ -57,8 +60,56 @@ describe 'ganglia::gmond' do
 
     let(:params) { params }
     it do
-      should contain_class('ganglia::gmond') 
-      should contain_file('/etc/ganglia/gmond.conf') 
+      should contain_class('ganglia::gmond')
+      should contain_file('/etc/ganglia/gmond.conf')
+    end
+  end
+
+  context 'with multicast channels' do
+    udp_recv_channel = [
+      {'port' => 8649, 'mcast_join' => '239.2.11.72', 'mcast_if' => 'eth0', 'bind' => '239.2.11.72'},
+    ]
+    udp_send_channel = [
+      {'port' => 8649, 'mcast_join' => '239.2.11.72', 'mcast_if' => 'eth0', 'ttl' => '1', 'bind_hostname' => 'yes'},
+    ]
+    tcp_accept_channel = [
+      {'port' => 8649},
+    ]
+    params = {
+      'cluster_name'        => "example grid",
+      'cluster_owner'       => "ACME, Inc.",
+      'cluster_latlong'     => "N32.2332147 W110.9481163",
+      'cluster_url'         => "www.example.org",
+      'host_location'       => "Example Computer Room",
+      'udp_recv_channel'    => udp_recv_channel,
+      'udp_send_channel'    => udp_send_channel,
+      'tcp_accept_channel'  => tcp_accept_channel,
+    }
+
+    let(:params) { params }
+    it do
+      should contain_class('ganglia::gmond')
+      should contain_file('/etc/ganglia/gmond.conf').
+        with_content(/^\s*bind_hostname\s+=\s+yes/).
+        with_content(/^\s*mcast_join\s+=\s+239\.2\.11\.72/).
+        with_content(/^\s*mcast_if\s+=\s+eth0/).
+        with_content(/^\s*port\s+=\s+8649/).
+        with_content(/^\s*ttl\s+=\s+1/)
+    end
+  end
+
+  context 'with gmond_package_name' do
+    gmond_package_name = [ 'ganglia-gmond', 'ganglia-gmond-python' ]
+    params = {
+      'gmond_package_name'  => gmond_package_name,
+    }
+
+    let(:params) { params }
+    it do
+      should contain_package('ganglia-gmond').
+        with_ensure('present')
+      should contain_package('ganglia-gmond-python').
+        with_ensure('present')
     end
   end
 end
