07201ccb6f224b3b492b09b0981616ab1e86dc9b
root <root@go2tech.de>
2016-07-14 23:00:35 +0200

    Remove iptables.local file
    Firewall rules are replaced by puppetlabs firewall module, which now
    dynamically manages the iptables rules on the hosts.
    
    Created some generic modules for services that need firewall fules, but
    don't have any puppetized parts, yet. This ensures, that the specific
    firewall rules are applied to the host (e.g. webserver or mailserver
    firewall rules).
    
    Change-Id: I7104a2d24df33d07588019d35f921aa32aa3381d

---
+++ modules/fw/manifests/post.pp
@@ -0,0 +1,56 @@
+# Post run class for iptables configuration
+class fw::post {
+  firewall { '993 log dropped input chain':
+    chain      => 'INPUT',
+    jump       => 'LOG',
+    log_level  => '6',
+    log_prefix => '[IPTABLES INPUT] dropped ',
+    proto      => 'all',
+    before     => undef,
+  }
+
+  firewall { '994 log dropped forward chain':
+    chain      => 'FORWARD',
+    jump       => 'LOG',
+    log_level  => '6',
+    log_prefix => '[IPTABLES FORWARD] dropped ',
+    proto      => 'all',
+    before     => undef,
+  }
+
+  firewall { '995 log dropped output chain':
+    chain      => 'OUTPUT',
+    jump       => 'LOG',
+    log_level  => '6',
+    log_prefix => '[IPTABLES OUTPUT] dropped ',
+    proto      => 'all',
+    before     => undef,
+  }
+
+  firewall { "996 deny all other input requests":
+    chain      => 'INPUT',
+    action     => 'drop',
+    proto      => 'all',
+    before     => undef,
+  }
+
+  firewall { '997 drop all input':
+    proto  => 'all',
+    action => 'drop',
+    before => undef,
+  }
+
+  firewall { '998 drop all forward':
+    proto  => 'all',
+    chain  => 'FORWARD',
+    action => 'drop',
+    before => undef,
+  }
+
+  firewall { '999 drop all output':
+    proto  => 'all',
+    chain  => 'OUTPUT',
+    action => 'drop',
+    before => undef,
+  }
+}
---
+++ modules/role/manifests/mailserver.pp
@@ -0,0 +1,16 @@
+# Generic class for a mailserver installation
+# currently only handles the firewall rules
+class role::mailserver {
+  firewall { '400 accept incoming mail traffic':
+    proto  => 'tcp',
+    dport  => ['25', '143', '587', '993'],
+    action => 'accept',
+  }
+
+  firewall { '401 accept outgoing mail traffic':
+    proto  => 'tcp',
+    dport  => ['25', '143', '587', '993'],
+    chain  => 'OUTPUT',
+    action => 'accept',
+  }
+}
