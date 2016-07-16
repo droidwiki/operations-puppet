643df07a7b620a19a28d0c9c2103ce23d1811b5e
Florian Schmidt <florian.schmidt.welzow@t-online.de>
2016-07-16 19:19:21 +0200

    Puppetize nginx and hhvm configuration
    One of the biggest parts of puppetizing the server setup.
    
    The new roles and configurations cover all virtual hosts, including
    location configuration, of all services provided by eclair.dwnet and
    donut.dwnet, but NOT the installation/configuration of the underlying
    services (such as shellinabox or mediawiki). Puppetizing these
    configuration will be part of separate commits for each service in the
    future.
    
    Change-Id: I0a8f9f15ef4a3f8ae011fbc73923a07b55a277dc

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,4 @@
+# site.pp
 node 'old_example' {
   include role::mediawiki
   include role::nginx::droidwiki
@@ -13,6 +14,19 @@ node 'eclair.dwnet' {
   include role::puppetmaster
   include role::deploymenthost
 
+  include role::nginx::go2tech
+  include role::nginx::blog_go2tech
+  include role::nginx::jenkins_go2tech
+  include role::nginx::droidwiki
+  include role::nginx::ops_go2tech
+  include role::nginx::dev_go2tech
+  include role::nginx::ganglia_go2tech
+  include role::nginx::gerrit_go2tech
+  include role::nginx::git_go2tech
+  include role::nginx::puppetboard_go2tech
+  include role::nginx::graphite_go2tech
+  include role::nginx::grafana_go2tech
+
   class { 'role::puppetboard':
     puppetboard_url => 'puppetboard.go2tech.de',
   }
@@ -26,4 +40,23 @@ node 'donut.dwnet' {
   include droidwiki::default
   include role::webserver
   include role::ganglia
+}
+
+node 'puppet.speedport.ip' {
+  # include droidwiki::default
+  include role::symlinkdata
+  include role::webserver
+  include hhvm
+  include role::nginx::go2tech
+  include role::nginx::blog_go2tech
+  include role::nginx::jenkins_go2tech
+  include role::nginx::droidwiki
+  include role::nginx::ops_go2tech
+  include role::nginx::dev_go2tech
+  include role::nginx::ganglia_go2tech
+  include role::nginx::gerrit_go2tech
+  include role::nginx::git_go2tech
+  include role::nginx::puppetboard_go2tech
+  include role::nginx::graphite_go2tech
+  include role::nginx::grafana_go2tech
 }
