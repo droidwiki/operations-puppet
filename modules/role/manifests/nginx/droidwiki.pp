755789843b16b0e2ce4c9615718bee3c55b606c1
Florian Schmidt <florian.schmidt.welzow@t-online.de>
2016-07-16 19:19:21 +0200

    Puppetize nginx and hhvm configuration
    One of the biggest parts of puppetizing the server setup.
    
    The new roles and configurations cover all virtual hosts, including
    location configuration, of all services provided by eclair.dwnet and
    donut.dwnet, but NOT the installation/configuration of the underlying
    services (such as shellinabox or mediawiki). Puppetizing these
    configuration will be part of separate commits for each service in the
    future.
    
    Change-Id: I0a8f9f15ef4a3f8ae011fbc73923a07b55a277dc

--- modules/role/manifests/nginx/droidwiki.pp
+++ modules/role/manifests/nginx/droidwiki.pp
@@ -1,7 +1,66 @@
 # nginx vhost droidwiki.de
 class role::nginx::droidwiki {
-  include role::nginx::ssl
-  nginx::site{ 'droidwiki.de':
-    content => template('droidwiki/droidwiki.conf.erb')
+  file { '/data/www/droidwiki.de':
+    ensure => 'directory',
+    owner  => 'www-data',
+    group  => 'www-data',
+    mode   => '0755',
+  }
+
+  file { '/data/www/droidwiki.de/public_html':
+    ensure => 'directory',
+    owner  => 'www-data',
+    group  => 'www-data',
+    mode   => '0755',
+  }
+
+  droidwiki::nginx::mediawiki { 'www.droidwiki.de':
+    vhost_url             => 'www.droidwiki.de',
+    server_name           => [ 'www.droidwiki.de' ],
+    manage_directories    => false,
+    html_root             => '/data/mediawiki/mw-config/mw-config/docroot',
+    listen_port           => 443,
+    ssl                   => true,
+    ssl_cert              => '/data/www/droidwiki.de/droidwiki.de.2017.crt',
+    ssl_key               => '/data/www/droidwiki.de/droidwiki.de.decrypted.key',
+    manage_http_redirects => false,
+    mediawiki_scriptpath  => '',
+    mediawiki_articlepath => '',
+  }
+
+  # some redirects
+  nginx::resource::vhost {
+    default:
+      listen_port          => 443,
+      ssl_port             => 443,
+      ssl                  => true,
+      ssl_cert             => '/data/www/droidwiki.de/droidwiki.de.2017.crt',
+      ssl_key              => '/data/www/droidwiki.de/droidwiki.de.decrypted.key',
+      ssl_dhparam          => $sslcert::params::dhparampempath,
+      http2                => on,
+      add_header           => {
+        'X-Delivered-By'            => $facts['fqdn'],
+        'Strict-Transport-Security' => '"max-age=31536000; includeSubdomains; preload"',
+      },
+      vhost_cfg_append => {
+        'return' => '301 https://www.droidwiki.de$request_uri',
+      },
+      use_default_location => false,
+    ;
+    'droidwiki.de':
+      server_name      => [ '.droid.wiki', '.droidwiki.de' ],
+      vhost_cfg_append => {
+        'return' => '301 https://www.droidwiki.de$request_uri',
+      },
+    ;
+    'droidwiki.de.80':
+      server_name      => [ '.droid.wiki', '.droidwiki.de' ],
+      listen_port      => 80,
+      ssl              => false,
+      add_header       => {
+        'X-Delivered-By' => $facts['fqdn'],
+      },
+      index_files      => [],
+    ;
   }
 }
---
+++ modules/sslcert/manifests/params.pp
@@ -0,0 +1,4 @@
+# sslcert parameters
+class sslcert::params {
+  $dhparampempath = '/etc/ssl/certs/dhparam.pem'
+}
