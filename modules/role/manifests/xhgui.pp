59f85a41dd67bf157f0ad36d3394d65c4f5afaf6
Florian Schmidt <florian.schmidt.welzow@t-online.de>
2016-08-27 22:50:05 +0200

    Add xhgui.go2tech.de as frontend for profiling
    Change-Id: Ied0ba5e71aa8c8fda468ed5e014200f54d9d41b6

---
+++ modules/role/manifests/xhgui.pp
@@ -0,0 +1,50 @@
+# Role for go2tech.de/xhgui
+# Should be used in combination with role::profilinghost
+class role::xhgui {
+  vcsrepo { '/data/xhprof':
+    ensure   => 'latest',
+    owner    => 'florian',
+    group    => 'florian',
+    provider => git,
+    require  => [ Package['git'] ],
+    source   => 'https://gerrit.wikimedia.org/r/operations/software/xhprof',
+    revision => 'wmf_deploy',
+  } ->
+
+  file { '/data/xhprof/profiles':
+    ensure => directory,
+    owner  => 'www-data',
+    group  => 'www-data',
+    mode   => '0755',
+  }
+
+  vcsrepo { '/data/xhgui':
+    ensure   => 'latest',
+    owner    => 'florian',
+    group    => 'florian',
+    provider => git,
+    require  => [ Package['git'] ],
+    source   => 'https://gerrit.wikimedia.org/r/operations/software/xhgui',
+    revision => 'wmf_deploy',
+  } ->
+
+  file { '/data/xhgui/cache':
+    ensure => directory,
+    owner  => 'www-data',
+    group  => 'www-data',
+    mode   => '0755',
+  } ->
+
+  droidwiki::nginx::hhvmvhost { 'xhgui.go2tech.de':
+    vhost_url => 'xhgui.go2tech.de',
+    www_root  => '/data/xhgui/webroot',
+  }
+
+  nginx::resource::location { "xhgui.go2tech.de/":
+    vhost                => 'xhgui.go2tech.de',
+    location             => '/',
+    location_custom_cfg => {
+      'try_files' => '$uri $uri/ /index.php?$uri&$args',
+    }
+  }
+}
