4085f900872d80f4feee45d735d3e21ab099dc00
Florian Schmidt <florian.schmidt.welzow@t-online.de>
2016-10-10 22:06:17 +0200

    Puppetize monit
    Enabled all services to donut.dwnet and installed and enabled
    services (which applies to the host) on eclair.dwnet.
    
    Change-Id: Id4c32e5777315a7447c32f1e70ea872bd4490b6e

---
+++ modules/monit/manifests/service.pp
@@ -0,0 +1,13 @@
+define monit::service() {
+  file { "/etc/monit/conf.d/${name}":
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template("monit/${name}.erb"),
+    notify  => Service['monit'],
+    require => [
+      File['conf.d'],
+      Package['monit']
+    ],
+  }
+}
