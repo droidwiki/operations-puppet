3b7fb10c5f6b5ba7217c9f15a3a1dd579cef4035
Florian Schmidt <florian.schmidt.welzow@t-online.de>
2016-10-10 22:35:38 +0200

    Monitor mariadb slave
    Change-Id: Ia1d48a2241e10afc8da0d2bd0b3db0c631f1d64f

--- modules/role/manifests/mariadb.pp
+++ modules/role/manifests/mariadb.pp
@@ -1,6 +1,8 @@
 # Manages automysqlbackup for now, will manage
 # mariadb in the future.
-class role::mariadb {
+class role::mariadb(
+  $isSlave = false,
+) {
   file { '/data/backup':
     ensure => 'directory',
     owner  => 'root',
@@ -23,5 +25,30 @@ class role::mariadb {
     table_exclude       => hiera( 'automysqlbackup::backup::table_exclude' ),
     mail_address        => hiera( 'automysqlbackup::backup::mail_address' ),
     mailcontent         => hiera( 'automysqlbackup::backup::mailcontent' ),
+  }
+
+  if ($isSlave) {
+    file { '/data/mariadb':
+      ensure => 'directory',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+    }
+
+    file { '/data/mariadb/slave_running.sh':
+      ensure => 'present',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0744',
+      source => 'puppet:///modules/role/mariadb/slave_running.sh',
+    }
+
+    cron { 'slave_running':
+      ensure  => 'present',
+      command => '/data/mariadb/slave_running.sh',
+      minute  => '*/1',
+    }
+
+    monit::service { 'mariadb_slave': }
   }
 }
