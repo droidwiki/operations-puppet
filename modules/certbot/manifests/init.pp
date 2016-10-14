b1d752ea94b5bb002b8c5fa8d03a9c98465ba86e
root <root@go2tech.de>
2016-10-14 20:26:31 +0200

    Add new certbot role for let's encrypt and try it on blog.go2tech.de
    This is just a test for now. Let's see if Let's encrypt can be handled for
    the little amount of sites on go2tech.de and droidwiki.de In best case,
    Let's encrypt will take over all certificates from Startcom.
    
    Change-Id: I70055e1982d78acee89454cfabbfac0ba4aaa114

---
+++ modules/certbot/manifests/init.pp
@@ -0,0 +1,20 @@
+# Module, which manages the certbot automatic renewel
+# of certificates. Unfortunately, currently does not
+# handle the the process of getting a new certificate.
+class certbot {
+  file { '/usr/local/sbin/certbot-auto':
+    ensure => 'present',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/certbot/certbot-auto',
+  }
+
+  cron { 'letsencrypt renew cron':
+    command => 'certbot-auto renew --quiet --no-self-upgrade',
+    user    => root,
+    hour    => 2,
+    minute  => 30,
+    weekday => 1,
+  }
+}
