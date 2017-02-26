4488d38ab442a8b89bdab45899e498cf07a6a8ca
root <root@go2tech.de>
2017-02-26 16:23:59 +0100

    Add missing monit manifest and template
    Change-Id: I59cc1b6a06896831d64fa3587f689966e9cab784

---
+++ modules/monit/manifests/certcheck.pp
@@ -0,0 +1,14 @@
+# Creates a monitored service file for a pre-defined template
+define monit::certcheck() {
+  file { "/etc/monit/conf.d/${name}":
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template("monit/certcheck.erb"),
+    notify  => Service['monit'],
+    require => [
+      File['conf.d'],
+      Package['monit']
+    ],
+  }
+}
