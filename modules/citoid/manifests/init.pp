8c259706a13ea2bf7bfe0417ff9cafae0577a8a8
root <root@go2tech.de>
2017-03-20 00:26:55 +0100

    WIP: Install own citoid instance
    Change-Id: I51141eb8a930e3251991b1cebba2316363a91fb7

---
+++ modules/citoid/manifests/init.pp
@@ -0,0 +1,59 @@
+# Manages the citoid service
+class citoid {
+  # install nodejs (will probably move to another module later)
+  apt::source { 'nodesource_nodejs':
+    comment  => 'NodeJS from nodesource',
+    location => 'https://deb.nodesource.com/node_7.x',
+    repos    => 'main',
+    key      => {
+      'id'     => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
+      'server' => 'http://pgp.mit.edu',
+    },
+  }
+
+  package { 'nodejs':
+    ensure => 'present',
+  }
+
+  vcsrepo { '/data/mediawiki/services/citoid':
+    ensure   => 'latest',
+    owner    => 'www-data',
+    group    => 'www-data',
+    provider => git,
+    require  => [ Package['git'] ],
+    source   => 'https://gerrit.wikimedia.org/r/mediawiki/services/citoid',
+    revision => 'master',
+    notify   => [ Service['citoid'], Exec['update-citoid-node_modules'] ],
+  }
+
+  exec { 'update-citoid-node_modules':
+    cwd     => '/data/mediawiki/services/citoid',
+    command => '/usr/bin/npm update',
+    user    => 'www-data',
+    group   => 'www-data',
+  }
+
+  file { '/etc/mediawiki/citoid.yaml':
+    ensure => 'present',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/citoid/citoid.yaml',
+    notify => Service['citoid'],
+  }
+
+  file { '/etc/init/citoid.conf':
+    ensure => 'present',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/citoid/citoid.init.conf',
+    notify => Service['citoid'],
+  }
+
+  service { 'citoid':
+    ensure  => 'running',
+    enable  => true,
+    require => Vcsrepo['/data/mediawiki/services/citoid'],
+  }
+}
