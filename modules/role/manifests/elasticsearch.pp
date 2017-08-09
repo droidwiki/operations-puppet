4b685187730e4bd1133466d727dcd93e5a787b70
root <root@go2tech.de>
2017-08-09 20:42:21 +0200

    Update ELK stask

--- modules/role/manifests/elasticsearch.pp
+++ modules/role/manifests/elasticsearch.pp
@@ -7,7 +7,7 @@ class role::elasticsearch {
   class { 'elasticsearch':
     manage_repo  => true,
     repo_version => '5.x',
-    version      => '5.1.2',
+    version      => '5.5.1',
     jvm_options  => [
       '-Xms1500m',
       '-Xmx1500m',
