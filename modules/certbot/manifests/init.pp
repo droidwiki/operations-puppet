562c5861c46ea2177caea0c68ad11e6302b35f4a
root <root@go2tech.de>
2018-02-17 13:34:46 +0100

    Update certbot puppet module to use ppa
    Ubuntu xenial has a ppa for installing certbot using apt. Use thsi instead
    of a manually copied version of certbot-auto (which was used for trusty before).

--- modules/certbot/manifests/init.pp
+++ modules/certbot/manifests/init.pp
@@ -2,16 +2,23 @@
 # of certificates. Unfortunately, currently does not
 # handle the the process of getting a new certificate.
 class certbot {
+  # remove old version of certbot for trusty - 17. Feb 2018
   file { '/usr/local/sbin/certbot-auto':
-    ensure => 'present',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/certbot/certbot-auto',
+    ensure => 'absent',
+  }
+
+  apt::ppa { 'ppa:certbot/certbot': }
+
+  package { 'certbot':
+    ensure  => 'present',
+    require => [
+      Apt::Ppa['ppa:certbot/certbot'],
+      Exec['apt_update'],
+    ]
   }
 
   cron { 'letsencrypt renew cron':
-    command => '/usr/local/sbin/certbot-auto renew --quiet --no-self-upgrade --renew-hook "service nginx restart"',
+    command => '/usr/bin/certbot renew --quiet --no-self-upgrade --renew-hook "service nginx restart"',
     user    => root,
     hour    => 2,
     minute  => 30,
