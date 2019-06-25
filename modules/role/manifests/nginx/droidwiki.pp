23d8672bce9dd5acebbc561f8f34b2da03ae42d0
root <root@go2tech.de>
2019-06-25 20:33:15 +0200

    Route traffic of droidwiki through varnish to cache static files

--- modules/role/manifests/nginx/droidwiki.pp
+++ modules/role/manifests/nginx/droidwiki.pp
@@ -27,16 +27,55 @@ class role::nginx::droidwiki {
     server_name           => [ 'www.droidwiki.org' ],
     manage_directories    => false,
     html_root             => '/data/mediawiki/mw-config/mw-config/docroot',
-    listen_port           => 443,
-    ssl                   => true,
-    ssl_cert              => $sslcert,
-    ssl_key               => $sslkey,
+    listen_port           => 80,
+    ssl                   => false,
+    http2                 => 'off',
     manage_http_redirects => false,
   }
 
   monit::certcheck { 'www.droidwiki.org': }
 
   $droidwiki_domains = [ '.droid.wiki', '.droidwiki.de', '.droid-wiki.org', '.droidwiki.org' ]
+
+  nginx::resource::server { 'www.droidwiki.org.external':
+    listen_port          => 443,
+    ipv6_enable          => true,
+    ipv6_listen_options  => '',
+    ssl                  => true,
+    ssl_port             => $ssl_port,
+    ssl_cert             => $sslcert,
+    ssl_key              => $sslkey,
+    ssl_dhparam          => $sslcert::params::dhparampempath,
+    http2                => 'on',
+    ssl_stapling         => true,
+    ssl_stapling_verify  => true,
+    use_default_location => false,
+    server_name          => [ 'www.droidwiki.org' ],
+    www_root             => '/data/mediawiki/main',
+    index_files          => [ 'index.php' ],
+    server_cfg_append    => {
+      'error_page 500 502 503 504' => '/500.html',
+      'error_page 403'             => '/403.html',
+      'gzip'                       => 'on',
+      'gzip_comp_level'            => '2',
+      'gzip_http_version'          => '1.0',
+      'gzip_proxied'               => 'any',
+      'gzip_min_length'            => '1100',
+      'gzip_buffers'               => '16 8k',
+      # disable gzip for IE < 6 because of known problems
+      'gzip_disable'               => '"MSIE [1-6].(?!.*SV1)"',
+      'gzip_vary'                  => 'on',
+    },
+    gzip_types           => 'text/plain text/css application/x-javascript text/xml application/xml application/xml+rss text/javascript',
+  }
+
+  nginx::resource::location { "www.droidwiki.org.external/":
+    location      => '/',
+    server        => 'www.droidwiki.org.external',
+    ssl           => true,
+    ssl_only      => true,
+    proxy         => 'http://172.16.0.1:6081',
+  }
 
   # some redirects
   nginx::resource::server {
--- modules/role/manifests/webserver.pp
+++ modules/role/manifests/webserver.pp
@@ -27,6 +27,16 @@ class role::webserver {
     provider => 'ip6tables',
   }
 
+  class { '::varnish':
+    secret      => hiera('varnish::secret'),
+    listen      => '0.0.0.0',
+    listen_port => 6081,
+  }
+
+  ::varnish::vcl { '/etc/varnish/default.vcl':
+    content => template('role/varnish/default.vcl.erb'),
+  }
+
   # deploy our own key exchange parameter for every webserver host
   class { 'sslcert::dhparam': }
 
