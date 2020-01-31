cbe9fd9b8919859152f80610390f48becba6d62e
Florian <florian.schmidt.welzow@t-online.de>
2020-01-31 19:06:35 +0100

    Install sync script

--- modules/role/manifests/backup_s3_sync.pp
+++ modules/role/manifests/backup_s3_sync.pp
@@ -7,4 +7,11 @@ class role::backup_s3_sync {
     aws_access_key_id     => hiera( 'backup_s3_sync::aws_access_key_id' ),
     aws_secret_access_key => hiera( 'backup_s3_sync::aws_secret_access_key' )
   }
+
+  file { '/data/backups/sync_backups.sh':
+    mode   => '0755',
+    group  => 'root',
+    owner  => 'root',
+    source => 'puppet:///modules/role/sync_backups.sh'
+  }
 }
