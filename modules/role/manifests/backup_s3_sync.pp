e7ffb22548243e831375a94e1db3f9d4a320319f
Florian <florian.schmidt.welzow@t-online.de>
2020-01-31 19:02:38 +0100

    Move to eu-west-1

--- modules/role/manifests/backup_s3_sync.pp
+++ modules/role/manifests/backup_s3_sync.pp
@@ -3,6 +3,7 @@
 class role::backup_s3_sync {
   class { 'awscli': }
   awscli::profile { 'backups':
+    aws_region            => 'eu-west-1',
     aws_access_key_id     => hiera( 'backup_s3_sync::aws_access_key_id' ),
     aws_secret_access_key => hiera( 'backup_s3_sync::aws_secret_access_key' )
   }
