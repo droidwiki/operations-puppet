c80773e065dfc969906713a46af5564a8cb2f2c3
Florian <florian.schmidt.welzow@t-online.de>
2020-01-31 19:36:09 +0100

    Less output

--- modules/role/files/sync_backups.sh
+++ modules/role/files/sync_backups.sh
@@ -4,6 +4,9 @@ set -e
 export AWS_PROFILE=backups
 
 currentDate=$(date +%F)
-aws s3 sync /data/backup/db/automysqlbackup/daily/ "s3://droidwiki-backups/${currentDate}/db/" --no-progress
-aws s3 sync /data/shareddata/mediawiki/images/ "s3://droidwiki-backups/${currentDate}/mw_images/" --exclude "temp/" --no-progress
-aws s3 sync /data/shareddata/www/missionrhode.go2tech.de/public_html/wp-content/uploads/ "s3://droidwiki-backups/${currentDate}/mr_wp/" --no-progress
+echo "Syncing db backups..."
+aws s3 sync /data/backup/db/automysqlbackup/daily/ "s3://droidwiki-backups/${currentDate}/db/" --no-progress --quiet
+echo "Syncing droidwiki images..."
+aws s3 sync /data/shareddata/mediawiki/images/ "s3://droidwiki-backups/${currentDate}/mw_images/" --exclude "temp/" --no-progress --quiet
+echo "Syncing missionrhode uploads..."
+aws s3 sync /data/shareddata/www/missionrhode.go2tech.de/public_html/wp-content/uploads/ "s3://droidwiki-backups/${currentDate}/mr_wp/" --no-progress --quiet
