3455d3f6d8f9eec5fd509ca6b22eadf78ff30551
Florian <florian.schmidt.welzow@t-online.de>
2020-02-10 00:46:57 +0100

    More cronjobs with php

--- modules/role/manifests/datawiki.pp
+++ modules/role/manifests/datawiki.pp
@@ -5,7 +5,7 @@ class role::datawiki {
   # This handles inserting jobs into client job queue, which then process the changes
   cron { 'datawiki-dispatch-changes':
     ensure  => present,
-    command => 'php /data/mediawiki/mw-config/mw-config/multiversion/MWScript.php extensions/Wikibase/repo/maintenance/dispatchChanges.php --wiki datawiki --max-time 540 --batch-size 275 --dispatch-interval 25 --lock-grace-interval 200 >/dev/null 2>&1',
+    command => 'docker run -it --rm --net=host -u www-data:www-data -v /data:/data -w /data/mediawiki droidwiki/php-fpm:7.4 php /data/mediawiki/mw-config/mw-config/multiversion/MWScript.php extensions/Wikibase/repo/maintenance/dispatchChanges.php --wiki datawiki --max-time 540 --batch-size 275 --dispatch-interval 25 --lock-grace-interval 200 >/dev/null 2>&1',
     user    => 'www-data',
     minute  => '*/3',
   }
@@ -13,7 +13,7 @@ class role::datawiki {
   # Prune wb_changes entries no longer needed from (test)wikidata
   cron { 'datawiki-prune':
     ensure  => present,
-    command => 'php /data/mediawiki/mw-config/mw-config/multiversion/MWScript.php extensions/Wikibase/repo/maintenance/pruneChanges.php --wiki datawiki --number-of-days=3 >> /data/log/mediawiki/prune.log 2>&1',
+    command => 'docker run -it --rm --net=host -u www-data:www-data -v /data:/data -w /data/mediawiki droidwiki/php-fpm:7.4 php /data/mediawiki/mw-config/mw-config/multiversion/MWScript.php extensions/Wikibase/repo/maintenance/pruneChanges.php --wiki datawiki --number-of-days=3 >> /data/log/mediawiki/prune.log 2>&1',
     user    => 'www-data',
     minute  => [0,15,30,45],
   }
